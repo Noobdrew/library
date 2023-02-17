199d8d52ef89fc9a438d7349d703875cfc42351e
Ivan Marinski <noobdrew23@gmail.com>
2023-02-17 16:47:29 +0200

    started constructor

--- script.js
+++ script.js
@@ -45,3 +45,15 @@ function removeBook(e) {
         }
     }
 }
+
+const library = []
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+}
+
