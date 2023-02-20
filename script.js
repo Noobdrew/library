335bda9b3301aab9a87eed723bdf15f6d6d182b6
Ivan Marinski <noobdrew23@gmail.com>
2023-02-20 15:50:36 +0200

    project complete
    value is stored in an array of objects that is synced with what the DOM shows, when a book is removed the object with the same index is deleted form the array

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const addBookBtn = document.querySelector('#addBook')
 const form = document.querySelector('#form')
 const overlay = document.querySelector('.overlay')
-const readBtn = document.querySelectorAll('.readBtn')
-const readBtnArr = Array.from(readBtn)
-const removeBtn = document.querySelectorAll('.remove')
-const removeBtnArr = Array.from(removeBtn)
-const cards = document.getElementsByClassName('card')
-const cardsArr = Array.from(cards)
+let readBtn = document.querySelectorAll('.readBtn')
+let readBtnArr = Array.from(readBtn)
+let removeBtn = document.querySelectorAll('.remove')
+let removeBtnArr = Array.from(removeBtn)
+let cards = document.getElementsByClassName('card')
+let cardsArr = Array.from(cards)
 const container = document.querySelector('.container')
 
-//add boock popup toggle
+//boock popup toggle
 addBookBtn.addEventListener('click', addBook)
 function addBook(e) {
     form.classList.add('form-active')
@@ -28,7 +28,12 @@ readBtnArr.forEach(element => {
     element.addEventListener('click', toggleRead)
 });
 function toggleRead(e) {
-    e.target.classList.toggle('read')
+    e.target.classList.toggle('true')
+    if (e.target.classList.contains('true')) {
+        library[parseInt(e.target.dataset.index)].read = true
+    } else {
+        library[parseInt(e.target.dataset.index)].read = false
+    }
 }
 
 //remove books
@@ -42,11 +47,21 @@ function removeBook(e) {
     for (const card of cards) {
         if (card.classList.contains('remove-card')) {
             card.remove()
+            delete library[parseInt(card.dataset.index)]
+
         }
     }
 }
 
+
 const library = []
+const submit = document.getElementById('submit')
+let bookCount = 0
+let title = document.querySelector('#title')
+let author = document.querySelector('#authour')
+let pages = document.querySelector('#pages')
+let readCheck = document.querySelector('#read')
+
 
 class Book {
     constructor(title, author, pages, read) {
@@ -56,4 +71,63 @@ class Book {
         this.read = read
     }
 }
+//store books into library array 
+function storeBook(e) {
+    if (title.value && author.value == '') {
+        return
+    }
+    e.preventDefault();
+    library[bookCount] = new Book(title.value, author.value, pages.value, readCheck.checked)
+    
+    //create card
+    createCard()
+    bookCount++
+    removePopup()
+    getDomElements()
+   
 
+
+}
+
+submit.addEventListener('click', storeBook)
+
+//create card function
+function createCard() {
+    const element = document.createElement('div')
+    element.classList.add('card')
+    element.dataset.index = bookCount
+    element.innerHTML = `
+    <div class="title">${title.value}</div>
+    <div class="authour">${author.value}</div>
+    <div class="pages">${pages.value}</div>
+    <button class="not-read readBtn ${readCheck.checked}" data-index='${bookCount}'></button>
+    <button class="remove">Remove book</button>
+    </div>`
+
+    document.querySelector('.container').appendChild(element)
+}
+
+//select all new created dom elements
+function getDomElements() {
+    readBtn = document.querySelectorAll('.readBtn')
+    readBtnArr = Array.from(readBtn)
+    removeBtn = document.querySelectorAll('.remove')
+    removeBtnArr = Array.from(removeBtn)
+    cards = document.getElementsByClassName('card')
+    cardsArr = Array.from(cards)
+    readBtnArr.forEach(element => {
+        element.addEventListener('click', toggleRead)
+    });
+    cardsArr.forEach(element => {
+        element.addEventListener('click', removeBook)
+    });
+}
+
+function removePopup() {
+    title.value = ''
+    author.value = ''
+    pages.value = ''
+    readCheck.checked = false
+    overlay.classList.remove('active')
+    form.classList.remove('form-active')
+}
